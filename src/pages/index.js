5a05f758a7cbe5fa3f434704c3f226a03689c68b
TahaAhmed63 <tahakap234@gmail.com>
2024-11-19 17:24:49 +0500

    yes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-import HomeBanner from '@/Components/Home/HomeBanner';
-import BriefProfile from '@/Components/Home/BreifProfile';
-import BlendingPlant from '@/Components/Home/BlendingPlant';
-import ExploreSolution from '@/Components/Home/ExploreSolution';
-import HomeVideoSec from '@/Components/Home/HomeVideoSec';
-import SVGComponent from '@/Components/Svgmapping/SVGComponent';
+import HomeBanner from './../Components/Home/HomeBanner';
+import BriefProfile from './../Components/Home/BreifProfile';
+import BlendingPlant from './../Components/Home/BlendingPlant';
+import ExploreSolution from './../Components/Home/ExploreSolution';
+import HomeVideoSec from './../Components/Home/HomeVideoSec';
+import SVGComponent from './../Components/Svgmapping/SVGComponent';
 //hdsfs
 // Dynamic import for client-only rendering
 const Product = dynamic(() => import('@/Components/Products/Product'), {
