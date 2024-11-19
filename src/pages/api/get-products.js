829143a3ee220f099ee8bc6f4ecb488a603c5569
TahaAhmed63 <tahakap234@gmail.com>
2024-11-20 00:14:45 +0500

    done

--- src/pages/api/get-products.js
+++ src/pages/api/get-products.js
@@ -20,7 +20,9 @@ const cache = {};
 
 export default async function handler(req, res) {
   const { perPage, page } = req?.query ?? {};
-
+  console.log("URL:", process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL);
+  console.log("Key:", process.env.WC_CONSUMER_KEY ? "Loaded" : "Missing");
+  console.log("Secret:", process.env.WC_CONSUMER_SECRET ? "Loaded" : "Missing");
   const cacheKey = `products-${perPage}-${page}`;
   if (cache[cacheKey]) {
     return res.json(cache[cacheKey]);
