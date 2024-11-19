1d122f8b8351dd63f6bf73b45c896e733d82ac7a
TahaAhmed63 <tahakap234@gmail.com>
2024-11-19 16:57:35 +0500

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,30 @@
-"use-client"
-import Image from "next/image";
-import localFont from "next/font/local";
-import Homepage from "./HomePage";
-import Header from "@/Components/Header/header";
-import Footer from "@/Components/Footer/footer";
+"use client";
+import React from 'react';
+import dynamic from 'next/dynamic';
+
+import HomeBanner from '@/Components/Home/HomeBanner';
+import BriefProfile from '@/Components/Home/BreifProfile';
+import BlendingPlant from '@/Components/Home/BlendingPlant';
+import ExploreSolution from '@/Components/Home/ExploreSolution';
+import HomeVideoSec from '@/Components/Home/HomeVideoSec';
+import SVGComponent from '@/Components/Svgmapping/SVGComponent';
+
+// Dynamic import for client-only rendering
+const Product = dynamic(() => import('@/Components/Products/Product'), {
+  ssr: false,
+});
+
 
 
 export default function Home() {
   return (
-   <>
-
-   <Homepage surpresshyderation/>
- 
-   </>
+    <>
+    <HomeBanner />
+    <BriefProfile />
+    <BlendingPlant />
+    <Product />
+    <ExploreSolution />
+    <HomeVideoSec />
+    <SVGComponent />
+  </>
   )}
